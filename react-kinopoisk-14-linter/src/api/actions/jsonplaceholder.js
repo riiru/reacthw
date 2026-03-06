2d5f2d2a3c46658c3cb1b6e0da278ddc836b87bd
riiru <sultanov.mps@mail.ru>
2026-03-06 10:53:13 +0300

    Delete react-kinopoisk-14-linter directory

--- react-kinopoisk-14-linter/src/api/actions/jsonplaceholder.js
+++
@@ -1,8 +0,0 @@
-import { getApiResource } from '../utils'
-
-export const getList = async () => {
-  const data = await getApiResource(
-    'https://jsonplaceholder.typicode.com/users',
-  )
-  return data
-}
--- react-kinopoisk-14-linter/src/api/constants.js
+++
@@ -1,7 +0,0 @@
-const API_KEY = import.meta.env.VITE_API_KEY
-
-export const API_BASE_URL = 'https://kinopoiskapiunofficial.tech'
-export const API_HEADERS = {
-  'X-API-KEY': API_KEY,
-  'Content-Type': 'application/json',
-}
